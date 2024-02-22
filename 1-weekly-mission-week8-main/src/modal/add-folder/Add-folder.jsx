0723e1fa8787b1a8190dfaa9318a3d4057f64a13
Jin-Chanyong <cksdyd324@gmail.com>
2024-02-22 19:04:57 +0900

    Feat: share, edit, add-folder 모달 형태 구현(기능 미구현)

--- 1-weekly-mission-week8-main/src/modal/add-folder/Add-folder.jsx
+++ 1-weekly-mission-week8-main/src/modal/add-folder/Add-folder.jsx
@@ -1,1 +1,19 @@
 import styles from "./Add-folder.scss";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
+
+export function AddFolder() {
+  return (
+    <div className={cx("background")}>
+      <div className={cx("modal")}>
+        <img className={cx("closeImg")} src="images/close.png" alt="close" />
+        <h2 className={cx("header")}>폴더 추가</h2>
+        <form className={cx("form")}>
+          <input className={cx("input")} placeholder="내용 입력"></input>
+          <button className={cx("button")}>추가하기</button>
+        </form>
+      </div>
+    </div>
+  );
+}
