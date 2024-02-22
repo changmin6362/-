e8c85dbe5860bd6bfffce087616abb038c42e265
KimChangmin <zzangmin3@gmail.com>
2024-02-22 16:04:41 +0900

    test1

--- 1-weekly-mission-week8-main/src/modal/delete-folder/Delete-folder.jsx
+++ 1-weekly-mission-week8-main/src/modal/delete-folder/Delete-folder.jsx
@@ -1,1 +1,15 @@
 import styles from "./Delete-folder.scss";
+import classNames from "classnames/bind";
+const cx = classNames.bind(styles);
+
+export const DeleteFolder = ({ selectedFolderId }) => {
+  return (
+    <div className={cx("container")}>
+      <form>
+        <p className={cx("label")}>폴더 삭제</p>
+        <p className={cx("props")}>{selectedFolderId}</p>
+        <button className={cx("button")}>삭제하기</button>
+      </form>
+    </div>
+  );
+};
