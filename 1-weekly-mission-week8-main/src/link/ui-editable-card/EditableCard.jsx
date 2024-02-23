fa632a24fa8ab5438260d0bc218cc3c64cf9b257
KimChangmin <zzangmin3@gmail.com>
2024-02-23 16:05:41 +0900

    mk modal delete-folde r& delete-link

--- 1-weekly-mission-week8-main/src/link/ui-editable-card/EditableCard.jsx
+++ 1-weekly-mission-week8-main/src/link/ui-editable-card/EditableCard.jsx
@@ -7,7 +7,14 @@ import { CardImage } from "sharing/ui-card-image";
 
 const cx = classNames.bind(styles);
 
-export const EditableCard = ({ url, imageSource, alt, elapsedTime, description, createdAt }) => {
+export const EditableCard = ({
+  url,
+  imageSource,
+  alt,
+  elapsedTime,
+  description,
+  createdAt,
+}) => {
   const [isHovered, setIsHovered] = useState(false);
   const handleMouseOver = () => setIsHovered(true);
   const handleMouseLeave = () => setIsHovered(false);
@@ -22,12 +29,19 @@ export const EditableCard = ({ url, imageSource, alt, elapsedTime, description, 
           createdAt={createdAt}
           isHovered={isHovered}
         />
-        <button className={cx("star")} onClick={(event) => event.preventDefault()}>
+        <button
+          className={cx("star")}
+          onClick={(event) => event.preventDefault()}
+        >
           <img src="images/star.svg" alt="즐겨찾기를 나타내는 별" />
         </button>
-        <button className={cx("kebab")} onClick={(event) => event.preventDefault()}>
+        <button
+          className={cx("kebab")}
+          onClick={(event) => event.preventDefault()}
+        >
           <img src="images/kebab.svg" alt="더보기를 나타내는 점 3개" />
         </button>
+        {isEnable && <SelectMenu url={url} mouseClick={handleMouseClick} />}
       </Card>
     </a>
   );
--- 1-weekly-mission-week8-main/src/modal/delete-link/Delete-link.jsx
+++ 1-weekly-mission-week8-main/src/modal/delete-link/Delete-link.jsx
@@ -1,1 +1,16 @@
 import styles from "./Delete-link.scss";
+import classNames from "classnames/bind";
+const cx = classNames.bind(styles);
+
+export const DeleteLink = ({ url, handleMouseClick }) => {
+  return (
+    <div className={cx("container")} onClick={handleMouseClick}>
+      <form className={cx("box")}>
+        <img src={"/images/_close.svg"} onClick={handleMouseClick} />
+        <p className={cx("label")}>링크 삭제</p>
+        <p className={cx("props")}>{url}</p>
+        <button className={cx("button")}>삭제하기</button>
+      </form>
+    </div>
+  );
+};
