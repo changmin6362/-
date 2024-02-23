0b9e96c530e3c159c3bf7c7e59f1020c4c0a2340
Jin-Chanyong <cksdyd324@gmail.com>
2024-02-23 16:12:22 +0900

    feat: share 모달 컴포넌트 facebook 공유, 복사 기능 추가

--- 1-weekly-mission-week8-main/src/modal/share/Share.jsx
+++ 1-weekly-mission-week8-main/src/modal/share/Share.jsx
@@ -8,6 +8,15 @@ export function Share({ folder }) {
     await navigator.clipboard.writeText(folder?.link);
   };
 
+  const handleFacebookClick = () => {
+    const sendUrl = `https://bootcamp-api.codeit.kr/shared/${folder?.id}`;
+    window.open("http://www.facebook.com/sharer/sharer.php?u=" + sendUrl);
+  };
+
+  const handleKakaoClick = () => {
+    return;
+  };
+
   return (
     <div className={cx("background")}>
       <div className={cx("modal")}>
@@ -18,11 +27,11 @@ export function Share({ folder }) {
         </div>
 
         <div className={cx("links")}>
-          <div className={cx("link")}>
+          <div className={cx("link")} onClick={handleKakaoClick}>
             <img src="images/kakao.png" alt="kakao" />
             <p>카카오톡</p>
           </div>
-          <div className={cx("link")}>
+          <div className={cx("link")} onClick={handleFacebookClick}>
             <img src="images/facebook2.png" alt="facebook" />
             <p>페이스북</p>
           </div>
