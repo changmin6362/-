dccfaa61fddbc6459c16c9ae1f13ee7012bb4103
KimChangmin <149885312+changmin6362@users.noreply.github.com>
2024-02-23 16:14:13 +0900

    Merge pull request #2 from Jin-Chanyong/day1-진찬용
    Day1 진찬용

--- 1-weekly-mission-week8-main/src/modal/edit/Edit.jsx
+++ 1-weekly-mission-week8-main/src/modal/edit/Edit.jsx
@@ -1,1 +1,19 @@
 import styles from "./Edit.scss";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
+
+export function Edit() {
+  return (
+    <div className={cx("background")}>
+      <div className={cx("modal")}>
+        <img className={cx("closeImg")} src="images/close.png" alt="close" />
+        <h2 className={cx("header")}>폴더 이름 변경</h2>
+        <form className={cx("form")}>
+          <input className={cx("input")} placeholder="유용한 팁"></input>
+          <button className={cx("button")}>변경하기</button>
+        </form>
+      </div>
+    </div>
+  );
+}
